3e3e6023450fd537144da40d7a38000cdae1bded
Norman Ainobushoborozi <norman.ainobushoborozi@gmail.com>
2025-01-20 22:07:37 +0300

    Error Removal

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -48,7 +48,7 @@ export default function Index() {
       <FlatList
         data={properties}
         renderItem={({item}) => <Card item={item} onPress={() => handleCardPress(item.$id)} />}
-        keyExtractor={(item) => item.toString()}
+        keyExtractor={(item) => item.$id}
         numColumns={2}
         contentContainerClassName="pb-32"
         columnWrapperClassName="flex gap-5 px-5"
@@ -90,7 +90,7 @@ export default function Index() {
               <FlatList
                 data={latestProperties}
                 renderItem={({item}) => <FeaturedCard item={item} onPress={() => handleCardPress(item.$id)} />}
-                keyExtractor={(item) => item.toString()}
+                keyExtractor={(item) => item.$id}
                 horizontal
                 bounces={false}
                 showsHorizontalScrollIndicator={false}
